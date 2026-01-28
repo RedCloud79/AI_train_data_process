71446b3e969078d560c3b4e7f93a55851fe45a0f
RedCloud79 <120488365+RedCloud79@users.noreply.github.com>
2026-01-28 20:51:17 +0900

    Update change_image_name.py

--- change_image_name.py
+++ change_image_name.py
@@ -7,5 +7,13 @@ Get-ChildItem -File -Filter *.png |
     $i++
   }
 
+$i = 1
+Get-ChildItem -File -Filter *.png |
+  Sort-Object Name |
+  ForEach-Object {
+    $newName = ("left_{0:00000}.png" -f $i)
+    Rename-Item -LiteralPath $_.FullName -NewName $newName
+    $i++
+  }
 
 ### power shell script
